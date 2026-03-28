5d2526538f4287d68243a1a9173b7db4f900846e
Daisy Koech <koechdaisy86@gmail.com>
2026-03-28 11:19:02 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// global scope 
+const burgers = ['Hamburger','Cheeseburger'];
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger () {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger)
+}
+    // newBurger = 'Flatburger'
+    // burgers.push(newBurger);
+    // console.log(burgers);
+
+// addBurger('Flatburger');
+// burgers.push('FlatBurger')
+// console.log(burgers);
+
+ if(true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink(drinkName) {
+    featuredDrink = drinkName;
+}
